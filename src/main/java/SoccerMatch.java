b2b8f44cdcbc7f7943cf7b2d742ef05ee1e86bdd
Brendan <brendanbath@gmail.com>
2020-04-07 03:00:36 +0200

    setup SoccerMatch test

---
+++ src/main/java/SoccerMatch.java
@@ -0,0 +1,10 @@
+public class SoccerMatch {
+    public SoccerMatch(SoccerTeam teamOne, SoccerTeam teamTwo, int scoreOne, int scoreTwo) {
+    }
+
+    public SoccerTeam getWinner() {
+        SoccerTeam winner = null;
+
+        return winner;
+    }
+}
---
+++ src/test/java/SoccerMatchTest.java
@@ -0,0 +1,104 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.Collection;
+import java.util.Random;
+import java.util.stream.Stream;
+
+class SoccerMatchTest {
+    private SoccerTeam teamOne, teamTwo;
+    private SoccerMatch soccerMatch;;
+
+    public void SoccerMatchTest() {
+        this.teamOne = new SoccerTeam("Test 1");
+        this.teamTwo = new SoccerTeam("Test 2");
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("teamOneWinIntIntProvider")
+    public void testTeamOneWin(int scoreOne, int scoreTwo) {
+        SoccerMatch tempSoccerMatch = new SoccerMatch(teamOne, teamTwo, scoreOne, scoreTwo);
+        SoccerTeam tempSoccerTeam = tempSoccerMatch.getWinner();
+        assertEquals(this.teamOne, tempSoccerTeam);
+    }
+
+    @ParameterizedTest
+    @MethodSource("teamTwoWinIntIntProvider")
+    public void testTeamTwoWin(int scoreOne, int scoreTwo) {
+        SoccerMatch tempSoccerMatch = new SoccerMatch(teamOne, teamTwo, scoreOne, scoreTwo);
+        SoccerTeam tempSoccerTeam = tempSoccerMatch.getWinner();
+
+        assertEquals(this.teamTwo, tempSoccerTeam);
+    }
+
+    @ParameterizedTest
+    @MethodSource("teamThreeWinIntIntProvider")
+    public void testDraw(int scoreOne, int scoreTwo) {
+        SoccerMatch tempSoccerMatch = new SoccerMatch(teamOne, teamTwo, scoreOne, scoreTwo);
+        SoccerTeam tempSoccerTeam = tempSoccerMatch.getWinner();
+
+        assertNull(tempSoccerTeam);
+    }
+
+    @ParameterizedTest
+    @MethodSource("illegalScoreProvider")
+    public void testInvalidScores(int scoreOne, int scoreTwo) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            SoccerMatch tempSoccerMatch = new SoccerMatch(teamOne, teamTwo, scoreOne, scoreTwo);
+        } );
+
+    }
+
+
+    static Stream<Arguments> teamOneWinIntIntProvider() {
+        Random r = new Random();
+        int upper = r.nextInt((10 - 6) + 1) + 6;
+        int lower = r.nextInt(6);
+        return Stream.of(
+                arguments(100, 0),
+                arguments(4, 2),
+                arguments(5,4),
+                arguments(2,0),
+                arguments(upper, lower)
+        );
+    }
+
+    static Stream<Arguments> teamTwoWinIntIntProvider() {
+        Random r = new Random();
+        int upper = r.nextInt((10 - 6) + 1) + 6;
+        int lower = r.nextInt(6);
+        return Stream.of(
+                arguments(0, 200),
+                arguments(1, 15),
+                arguments(2,4),
+                arguments(0, 10),
+                arguments(lower, upper)
+        );
+    }
+
+    static Stream<Arguments> teamThreeWinIntIntProvider() {
+        Random r = new Random();
+        int value = r.nextInt(11);
+        return Stream.of(
+                arguments(100, 0),
+                arguments(4, 2),
+                arguments(5,4),
+                arguments(2,0),
+                arguments(value, value)
+        );
+    }
+
+    static Stream<Arguments> illegalScoreProvider() {
+        return Stream.of(
+                arguments(-1, 10),
+                arguments(-10, -5),
+                arguments(10,-5)
+        );
+    }
+}
